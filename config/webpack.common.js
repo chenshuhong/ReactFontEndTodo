3ae094c75e433b7a985cd3b108631182599e162a
baiye <baiye.csh@dtyunxi.com>
2018-10-05 16:48:04 +0800

    build(项目搭建):

---
+++ config/webpack.common.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'React Todo List',
+      template: './src/index.html'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.(css|less)$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'postcss-loader',
+        'less-loader'
+      ]
+    }, {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel-loader"
+    }]
+  }
+};
