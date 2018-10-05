735c3099a263b981a3d551c181cf24d63cafbe4d
baiye <baiye.csh@dtyunxi.com>
2018-10-05 18:25:17 +0800

    build(项目搭建):

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * @Author: 陈树鸿
+ * @Date: 2018--05
+ */
+import ReactDom from 'react-dom'
+import React from "react";
+const element = (
+  <div>hello react</div>
+)
+ReactDom.render(element,document.getElementById('root'))
